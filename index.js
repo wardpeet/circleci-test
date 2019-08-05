487a373ff4e7d73018baf8b7016cc68d076efb7e
Ward Peeters <ward@coding-tech.com>
2019-08-05 15:04:14 +0200

    newlines

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ console.log('ANALYTICS_SERVICE_ACCOUNT_KEY', process.env.ANALYTICS_SERVICE_ACCOU
 
 console.log('~~~ CIRCLE CI ~~~')
 console.log('========================')
-console.log('ANALYTICS_SERVICE_ACCOUNT', process.env.CI_ANALYTICS_SERVICE_ACCOUNT)
-console.log('ANALYTICS_SERVICE_ACCOUNT_KEY', process.env.CI_ANALYTICS_SERVICE_ACCOUNT_KEY)
+console.log('ANALYTICS_SERVICE_ACCOUNT', process.env.CI_ANALYTICS_SERVICE_ACCOUNT.replace(/\\n/g, '\n'))
+console.log('ANALYTICS_SERVICE_ACCOUNT_KEY', process.env.CI_ANALYTICS_SERVICE_ACCOUNT_KEY.replace(/\\n/g, '\n'))
